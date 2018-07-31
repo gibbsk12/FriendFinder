ee65adad20fe4d8b8e2a0c1adc44e752870ae8de
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-07-31 17:48:35 -0400

    local hosting working

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+//ROUTER
+// require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
