4e1581347a3ff3ac7ac1c629cb1c2962448bfc9d
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-08-02 00:11:19 -0400

    functioning locally

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,22 +3,34 @@ var friends = require('../data/friends');
 
 module.exports = function (app) {
 
-    app.get('/api/friends', function (req, res) {
-        res.json(friends);
+    app.get('/api/friends', function (request, response) {
+        response.json(friends);
     })
 
-    //To add new entry
     app.post('/api/friends', function (request, response) {
         var surveyData = request.body;
-        console.log(surveyData)
-        console.log(friends)
-        var questionScores = surveyData.scores;
+		var name;
+		var photo;
+		var totalDifference = 10000; 
 
+		for (var i = 0; i < friends.length; i++) {
+			var diff = 0;
+			for (var j = 0; j < surveyData.scores.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - surveyData.scores[j]);
+                console.log(diff);
+			}
+			if (diff < totalDifference) {
+				totalDifference = diff;
+				name = friends[i].name;
+				photo = friends[i].photo;
+			}
+		}
 
+		friends.push(surveyData);
 
-        
-        friends.push(surveyData)
+		response.json({name:name, photo: photo});
     });
 };
+
 
 
